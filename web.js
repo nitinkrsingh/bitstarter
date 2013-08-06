34d8f57dc637eb9dab4a2765074522595eabe8ad
nitin.singh <ubuntu@ip-172-31-33-162.us-west-2.compute.internal>
2013-08-06 22:19:40 +0000

    modified to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var myString = fs.readFileSync("index.html").toString();
+  response.send(myString);
 });
 
 var port = process.env.PORT || 5000;
